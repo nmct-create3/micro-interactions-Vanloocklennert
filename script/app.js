e018552e4dbc2339e49c2cd3b61b5fc1ce3c2cdf
Lennert van Loock <lennertvanloock@outlook.com>
2020-10-08 16:06:10 +0200

    labo01

--- script/app.js
+++ script/app.js
@@ -2,8 +2,22 @@ function handleFloatingLabel() {
    
 }
 
-function handlePasswordSwitcher() {
-    
+const handlePasswordSwitcher = function() {
+    const passwordInput = document.querySelector('.js-password-input'),
+    passwordToggle = document.querySelector('.js-password-toggle');
+
+    if (!passwordInput || !passwordToggle){
+        console.error('De classen werden niet gevonden.',passwordInput,passwordToggle);
+        return;
+    }
+    passwordToggle.addEventListener('change',function(){
+        if(passwordInput.type == 'password'){
+            passwordInput.type= 'text';
+        } 
+        else{
+            passwordInput.type = 'password';
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
---
+++ script/validation.js
@@ -0,0 +1,15 @@
+let email = {},
+    password = {},
+    signInButton;
+
+const getDOMElements = function(){
+    
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Script loaded!');
+    getDOMElements();
+});
